7a9972b382a70d05c6a6d9ff4633e2dfde5654a5
Codexzier <johannes.paul.langner@live.de>
2018-09-23 11:21:03 +0200

    Beispiel sichern

--- ExamplePca9685PwmDriver/Pca9685.cs
+++ ExamplePca9685PwmDriver/Pca9685.cs
@@ -1,3 +1,29 @@
+/// #########################################################################################################
+///
+///  Blog: Meine Welt in meinem Kopf
+///  Post: 16 Chanel Pwm Driver mit dem Netduino
+///  Postdate: 20.09.2018
+///  --------------------------------------------------------------------------------------------------------
+///  Kurze Information:
+///  Diese Solution dient als Quellcode Beispiel und zeigt lediglich 
+///  die Funktionsweise für das Initialisieren des Sensors und abruf der Daten.
+///  Fehlerbehandlung, sowie Logging oder andere Erweiterungen 
+///  für eine stabile Laufzeit der Anwendung sind nicht vorhanden.
+///  
+///  Für Änderungsvorschläge oder ergänzende Informationen zu meiner
+///  Beispiel Anwendung, der oder die kann mich unter der Mail Adresse 
+///  j.langner@gmx.net erreichen.
+///  
+///  Referenzen:
+///  https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library/blob/master/Adafruit_PWMServoDriver.cpp
+///  https://cdn-shop.adafruit.com/datasheets/PCA9685.pdf
+///  
+///  Vorraussetzung:
+///  Netduino
+///  PCA9685 16 channel pwm driver
+/// 
+/// #########################################################################################################
+
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using System;
